d92e2bbdc09f42f0b215b0f2175f0ce76d4b2e69
Remy Paul <remypauljrcloud@gmail.com>
2024-04-24 15:37:40 -0400

    Updating JS into HTML file

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
-const counter = document.querySelector(".visitorCounter") 
-async function updateCounter(){
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+})
 
-    let response = await fetch("https://o9h7o4om9j.execute-api.us-east-1.amazonaws.com/visitorCounter");
-    let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`;
+const functionApi = 'https://o9h7o4om9j.execute-api.us-east-1.amazonaws.com/visitorCounter';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response => {
+        console.log("Website called by API");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
 }
-
-updateCounter();
