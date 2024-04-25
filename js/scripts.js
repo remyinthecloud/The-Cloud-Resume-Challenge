e353598efd2c39e4ce2f62d153bd9449569cc1c6
Remy Paul <remypauljrcloud@gmail.com>
2024-04-24 21:35:57 -0400

    Modifying js fetch to lamba function url

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://o9h7o4om9j.execute-api.us-east-1.amazonaws.com/Origin/visitorCounter");
+    let response = await fetch("https://nxvtfqdljcrvm4tn4tv4snjppa0yylyn.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
 }
