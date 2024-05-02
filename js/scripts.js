63172c6091713c3f5c186de4e2f5b7efa8b4e783
Remy Paul <remypauljrcloud@gmail.com>
2024-05-01 23:39:16 -0400

    Final commit for js file to show visitor count after debugging

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,5 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://nxvtfqdljcrvm4tn4tv4snjppa0yylyn.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    print(data)
     counter.innerHTML = ` This page has ${data} Views!`;
 }
